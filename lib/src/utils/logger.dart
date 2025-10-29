3e94375a8271020021717ebb9991aec27035b0bc
Resam Taghipour <resam.t@gmail.com>
2025-10-29 15:26:57 +0330

    Defaults and Utils

--- lib/src/utils/logger.dart
+++ lib/src/utils/logger.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package_defaults.dart';
 
-debugLog(String log, {bool disabled = false}) => disabled == true ? null : debugPrint(log);
-releaseLog(String log, {bool disabled = false}) => disabled == true ? null : print(log);
+debugLog(String log, {bool disabled = false}) =>
+    disabled == true && PackageDefaults.printLogs != true ? null : debugPrint(log);
+
+releaseLog(String log, {bool disabled = false}) =>
+    disabled == true && PackageDefaults.printLogs != true ? null : print(log);
---
+++ lib/src/utils/package_defaults.dart
@@ -0,0 +1,5 @@
+class PackageDefaults {
+  static bool get drawDocumentBorder => false;
+  static bool get printLogs => true;
+
+}
