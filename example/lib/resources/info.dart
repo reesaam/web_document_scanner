df89b5b980342fe0c3fa52cf2ebee54fcc60cca0
Resam Taghipour <resam.t@gmail.com>
2025-10-30 10:40:25 +0330

    Example Implementation

---
+++ example/lib/resources/info.dart
@@ -0,0 +1,12 @@
+class PackageAuthorInfo {
+  static String get packageName => 'getx_binding_annotation';
+  static String get packageNameDescription => 'Getx Dependencies Binding Annotation and Generator';
+  static String get packageAddress => 'https://pub.dev/packages/getx_binding_annotation';
+  static String get packageGitHubRepositoryAddress => 'https://github.com/reesaam/flutter_getx_dependencies_binding_annotation';
+  static String get authorName => 'Resam Taghipour';
+  static String get emailAddress => 'resam.t@gmail.com';
+  static String get websiteAddress => 'https://www.resam-t.ir';
+  static String get linkedinAddress => 'https://www.linkedin.com/in/resam';
+  static String get githubAddress => 'https://github.com/reesaam';
+  static String get version => '0.2.5';
+}
--- lib/src/utils/package_defaults.dart
+++
@@ -1,6 +0,0 @@
-class PackageDefaults {
-  static bool get drawDocumentBorder => false;
-  static bool get printDebugLogs => true;
-  static bool get printReleaseLogs => true;
-  static int get constCropAmount => 20;
-}
